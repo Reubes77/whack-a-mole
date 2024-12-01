2b574e2cf03358a95454bd1e4d5f7149fda2ec5d
Reuben Abrahams <oaxsvin4@students.codeinstitute.net>
2024-12-01 16:05:02 +0000

    Add basic structure to game functions

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+
+    // Random select hole function
+    function randomHole() {
+      
+    }
+
+    // Function to make mole pop up in hole
+    function showMole() {
+    
+    }
+
+    // Start game function
+    function startGame() {
+   
+    }
+
+    // Function to whack mole
+    function whackMole(event) {
+
+    }
+
+    // Function to update Timer
+    function updateTimer() {
+        
+        // Update dispay timer
+       
+        // Change color of timer when reaching 5 seconds or less
+        
+    }
+
+    // Function to end game
+    function endGame() {
+        
+    }
+
+    // Attach start game to button
+  
+});
