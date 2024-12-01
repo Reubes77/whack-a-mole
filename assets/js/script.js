3f06e52520628965f8ac1c89f1c0a2c7ed6a2df8
Reuben Abrahams <oaxsvin4@students.codeinstitute.net>
2024-12-01 18:41:11 +0000

    Add hole selection function to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+    const holes = document.querySelectorAll(".hole"); // Select all holes   
+    const moles = document.querySelectorAll(".mole"); // Select all moles
+    const scoreDisplay = document.getElementById("score"); // Displays score
+    const startButton = document.getElementById("start-button"); // Start button
+    let score = 0;
+    let lastHole;
+    let gameInterval;
+    let timerInterval;
+    let gameTime = 30; // 30 second game duration
+    let timeRemaining = gameTime;
 
     // Random select hole function
     function randomHole() {
-      
+        const index = Math.floor(Math.random() * holes.length);
+        const selectedHole = holes[index];
+
+        // Prevent same hole being selected consecutively
+        if (selectedHole === lastHole) {
+            return randomHole();
+        }
+        lastHole = selectedHole;
+        return selectedHole;      
     }
 
     // Function to make mole pop up in hole
